14fbf1a1761de0c723f7db5bada714f5c27bd148
Jose Manuel <jm.maya@iesnervion.es>
2025-10-09 09:56:12 +0200

    update

--- Tema1/segundo_boletin_mayahidalgo_josemanuel.sql
+++ Tema1/segundo_boletin_mayahidalgo_josemanuel.sql
@@ -336,3 +336,68 @@ end;
 
 exec mostrar_notas_por_modulo 'RET';
 
+
+
+/*Ejercicio 3*/
+
+create database tienda
+use tienda
+
+CREATE TABLE productos
+(
+	CodProducto VARCHAR(10) CONSTRAINT p_cod_no_nulo NOT NULL,
+	Nombre VARCHAR(20) CONSTRAINT p_nom_no_nulo NOT NULL,
+	LineaProducto VARCHAR(10),
+	PrecioUnitario NUMERIC(6, 0),
+	Stock INT,
+	CONSTRAINT PK_productos PRIMARY KEY (CodProducto)
+);
+
+CREATE TABLE ventas
+(
+	CodVenta VARCHAR(10) CONSTRAINT cod_no_nula NOT NULL,
+	CodProducto VARCHAR(10) CONSTRAINT pro_no_nulo NOT NULL,
+	FechaVenta DATE,
+	UnidadesVendidas TINYINT,
+	CONSTRAINT PK_ventas PRIMARY KEY (CodVenta),
+	CONSTRAINT FK_ventas_productos FOREIGN KEY (CodProducto) REFERENCES productos(CodProducto)
+);
+
+INSERT INTO productos (CodProducto, Nombre, LineaProducto, PrecioUnitario, Stock) VALUES ('1','Procesador P133', 'Proc',15000,20);
+INSERT INTO productos (CodProducto, Nombre, LineaProducto, PrecioUnitario, Stock) VALUES ('2','Placa base VX', 'PB', 18000,15);
+INSERT INTO productos (CodProducto, Nombre, LineaProducto, PrecioUnitario, Stock) VALUES ('3','Simm EDO 16Mb', 'Memo', 7000,30);
+INSERT INTO productos (CodProducto, Nombre, LineaProducto, PrecioUnitario, Stock) VALUES ('4','Disco SCSI 4Gb', 'Disc',38000, 5);
+INSERT INTO productos (CodProducto, Nombre, LineaProducto, PrecioUnitario, Stock) VALUES ('5','Procesador K6-2', 'Proc',18500,10);
+INSERT INTO productos (CodProducto, Nombre, LineaProducto, PrecioUnitario, Stock) VALUES ('6','Disco IDE 2.5Gb', 'Disc',20000,25);
+INSERT INTO productos (CodProducto, Nombre, LineaProducto, PrecioUnitario, Stock) VALUES ('7','Procesador MMX', 'Proc',15000, 5);
+INSERT INTO productos (CodProducto, Nombre, LineaProducto, PrecioUnitario, Stock) VALUES ('8','Placa Base Atlas','PB', 12000, 3);
+INSERT INTO productos (CodProducto, Nombre, LineaProducto, PrecioUnitario, Stock) VALUES ('9','DIMM SDRAM 32Mb', 'Memo',17000,12);
+GO
+
+SET DATEFORMAT DMY;
+
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V1', '2', '22/09/97',2);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V2', '4', '22/09/97',1);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V3', '6', '23/09/97',3);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V4', '5', '26/09/97',5);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V5', '9', '28/09/97',3);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V6', '4', '28/09/97',1);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V7', '6', '02/10/97',2);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V8', '6', '02/10/97',1);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V9', '2', '04/10/97',4);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V10','9', '04/10/97',4);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V11','6', '05/10/97',2);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V12','7', '07/10/97',1);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V13','4', '10/10/97',3);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V14','4', '16/10/97',2);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V15','3', '18/10/97',3);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V16','4', '18/10/97',5);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V17','6', '22/10/97',2);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V18','6', '02/11/97',2);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V19','2', '04/11/97',3);
+INSERT INTO ventas (CodVenta, CodProducto, FechaVenta, UnidadesVendidas) VALUES('V20','9', '04/12/97',3);
+
+
+
+
+
